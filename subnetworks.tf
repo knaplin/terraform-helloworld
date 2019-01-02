32871837c9b90d04f1ee2f41440f2548806c31b2
Napon Linlawan <naponknot@gmail.com>
2019-01-02 09:46:08 +0700

    add server

--- subnetworks.tf
+++ subnetworks.tf
@@ -1,12 +1,3 @@
-resource "aws_vpc" "environment-example" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  tags {
-    Name = "terraform-aws-vpc-example"
-  }
-}
-
 resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.environment-example.id}"
@@ -32,4 +23,3 @@ resource "aws_security_group" "subnetsecurity" {
     protocol = "tcp"
   }
 }
-
